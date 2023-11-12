4327347707394b6ba4de60c70e686cd0b9f724a3
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-11-12 05:43:06 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+echo "CLIENTE:<br/>";
+echo $_SERVER["REMOTE_ADDR"]."<br/>";  //Imprime la dirección IP del cliente
+echo $_SERVER['HTTP_USER_AGENT']."<br/>"; /*Imprime la información de S.O y navegador del cliente*/
+echo $_SERVER["REMOTE_PORT"]."<br/>"; //Imprime el puerto del cliente
+echo "<script> document.write(window.innerWidth+'x'+window.innerHeight); </script><br/><br/>"; //Imprime tamaño de pantalla
+
+echo "SERVIDOR:<br/>";
+echo $_SERVER['SERVER_ADDR']."<br/>"; //Imprime la IP del servidor
+echo $_SERVER['SERVER_NAME']."<br/>"; //Imprime el nombre del servidor
+echo $_SERVER['SERVER_SOFTWARE']."<br/>"; //Imprime el software que usa el servidor
+echo $_SERVER['SERVER_PROTOCOL']."<br/>"; //Imprime el protocolo usado
+echo $_SERVER['REQUEST_METHOD']."<br/>"; //Imprime el método de petición empleado
+echo $_SERVER['REQUEST_TIME']."<br/>";  //Imprime el tiempo de respuesta
+?>
